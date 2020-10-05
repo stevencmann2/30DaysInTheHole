2e2198aaf2cf4129be99935ca695f2a1f743ce09
Steven Mann <stevencmann2@gmail.com>
2020-10-05 10:44:02 -0500

    adding code for day 6 , review loop

---
+++ Javascript/day6.js
@@ -0,0 +1,24 @@
+function processData(input) {
+    //Enter your code here
+    let words = input.split('\n');
+
+    for(let i = 1; i < words.length; i++){
+        let str = words[i];
+        let letters = str.split('');
+        let evenArr = [];
+        let oddArr = [];
+            for (let j = 0; j < letters.length; j++){
+                if (j === 0 || j % 2 === 0){
+                    evenArr.push(letters[j])
+                }else{
+                    oddArr.push(letters[j])
+                }
+            }
+            let evenStr = evenArr.join('');
+            let oddStr = oddArr.join('')
+            console.log(`${evenStr} ${oddStr}`) 
+    }
+
+} 
+
+
