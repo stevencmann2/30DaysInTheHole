7448fa0667b53a96d80d3b2e71f1cc4b0f08feb5
Steven Mann <stevencmann2@gmail.com>
2020-10-21 13:34:28 -0500

    deleting comments from day 10

--- Javascript/day10.js
+++ Javascript/day10.js
@@ -28,17 +28,3 @@ binaryConverter(65535)
 binaryConverter(439)
 
 
-// NEED TO FIND CASE WHERE LENGTH -1 FOR LAST SET OF NUMBERS BEING THE FINAL COUNT 
-
-  //   for (i = 0; i < binaryArr.length - 1; i++) {
-    //     // counter =1
-    //     if (binaryArr[i] === '1') {
-    //         onesCounter++;
-    //   }else if(binaryArr[i]=== '0') {
-    //       if (onesCounter > consecutiveOnes){
-    //         consecutiveOnes = onesCounter;
-    //         onesCounter = 0;
-    //       }
-    //     }  
-  
-    // }
